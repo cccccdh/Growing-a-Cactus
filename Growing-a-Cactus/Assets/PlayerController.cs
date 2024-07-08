8009a9a3e6f9278bbb576cbf97d2e652163e582b
cccccdh <choidh0930@naver.com>
2024-07-08 12:19:43 +0900

    초기 파일 생성

---
+++ Growing-a-Cactus/Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 스탯
+    // 체력 바
+    // 일정 거리 내에 있을 때 적 타겟팅 해서 공격
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
